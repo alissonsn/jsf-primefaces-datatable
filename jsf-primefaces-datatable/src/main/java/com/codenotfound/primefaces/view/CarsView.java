aaed2532b16b3bb3ba02388f512e4083f818cb10
Alisson Nascimento <alissonsn@gmail.com>
2018-09-06 15:36:19 -0300

    Commitando

--- jsf-primefaces-datatable/src/main/java/com/codenotfound/primefaces/view/CarsView.java
+++ jsf-primefaces-datatable/src/main/java/com/codenotfound/primefaces/view/CarsView.java
@@ -11,8 +11,11 @@ import javax.inject.Named;
 import com.codenotfound.primefaces.model.Car;
 import com.codenotfound.primefaces.repository.CarRepository;
 
+import lombok.Data;
+
 @Named
 @ViewScoped
+@Data
 public class CarsView implements Serializable {
 
   private static final long serialVersionUID = 1L;
@@ -20,11 +23,18 @@ public class CarsView implements Serializable {
   @Inject
   private CarRepository carRepository;
 
+  private Car car;
+  
   private List<Car> cars;
 
   @PostConstruct
   public void init() {
+	car = new Car();
     cars = carRepository.findAll();
+  }
+  
+  public void cadastrar() {
+	  carRepository.save(car);
   }
 
   public List<Car> getCars() {
